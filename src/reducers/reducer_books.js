375c86d3c6e24ff3ba8a721d0d845354234346fd
Cristhian Cuenca <ccuenca24@gmail.com>
2018-05-02 14:42:15 -0500

    initial commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,10 @@
+
+export default function() {
+    return [
+        { title: 'Javascrip: The Goods Parts', pages: 90},
+        { title: 'Lord of the Rings I', pages: 80},
+        { title: 'Lord of the Rings II', pages: 70},
+        { title: 'Toy Story I', pages: 60},
+        { title: 'Toy Story II', pages: 50}
+    ];
+}
